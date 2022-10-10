862953d9b5796f030d16c253a734ac4aa4e74a4f
Harold Jiang <jzsues@gmail.com>
2022-10-10 12:10:21 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,7 +21,8 @@ PACKAGES="$PACKAGES luci-i18n-cpulimit-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-statistics-zh-cn"
 
 # 常用软件服务
-
+# upnp
+PACKAGES="$PACKAGES luci-app-upnp"
 # OpenClash 代理
 PACKAGES="$PACKAGES luci-app-openclash"
 # Passwall 代理
@@ -32,31 +33,31 @@ PACKAGES="$PACKAGES luci-i18n-nps-zh-cn luci-i18n-frpc-zh-cn"
 # 常用的网络存储组件
 
 # 文件助手
-PACKAGES="$PACKAGES luci-app-fileassistant"
+# PACKAGES="$PACKAGES luci-app-fileassistant"
 # 硬盘休眠
-PACKAGES="$PACKAGES luci-i18n-hd-idle-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-hd-idle-zh-cn"
 # p910nd 打印服务器
-PACKAGES="$PACKAGES luci-i18n-p910nd-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-p910nd-zh-cn"
 # GoWebDav
-PACKAGES="$PACKAGES luci-i18n-gowebdav-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-gowebdav-zh-cn"
 # Samba 网络共享
-PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 # qBittorrent 下载器
-PACKAGES="$PACKAGES luci-i18n-qbittorrent-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-qbittorrent-zh-cn"
 # Aria2 下载器
-PACKAGES="$PACKAGES luci-i18n-aria2-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-aria2-zh-cn"
 # FTP 服务器
-PACKAGES="$PACKAGES luci-i18n-vsftpd-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-vsftpd-zh-cn"
 
 # Docker 组件
-PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn luci-i18n-docker-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn luci-i18n-docker-zh-cn"
 
 # VPN 类
 
 # IPSec VPN
-PACKAGES="$PACKAGES luci-i18n-ipsec-vpnd-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-ipsec-vpnd-zh-cn"
 # Zerotier
-PACKAGES="$PACKAGES luci-i18n-zerotier-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-zerotier-zh-cn"
 
 # 网络组件
 
@@ -74,7 +75,7 @@ PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn luci-i18n-base-zh-cn luci-i18n-firewall
 PACKAGES="$PACKAGES luci-i18n-wrtbwmon-zh-cn"
 
 # 一些自己需要的内核组件
-PACKAGES="$PACKAGES kmod-usb-printer kmod-lp"
+# PACKAGES="$PACKAGES kmod-usb-printer kmod-lp"
 
 # 一些其他可能有用的包
 
@@ -85,7 +86,7 @@ PACKAGES="$PACKAGES vim-fuller"
 # X/Y/ZMODEM 文件传输
 PACKAGES="$PACKAGES lrzsz"
 # OpenSSH
-PACKAGES="$PACKAGES openssh-server openssh-client"
+# PACKAGES="$PACKAGES openssh-server openssh-client"
 # Netdata 系统监控界面
 PACKAGES="$PACKAGES netdata"
 
@@ -93,6 +94,6 @@ PACKAGES="$PACKAGES netdata"
 FILES="files"
 
 # 禁用 openssh-server 的 sshd 服务和 docker 的 dockerd 服务以防止冲突
-DISABLED_SERVICES="sshd dockerd"
+# DISABLED_SERVICES="sshd dockerd"
 
 make image PROFILE="$PROFILE" PACKAGES="$PACKAGES" FILES="$FILES" DISABLED_SERVICES="$DISABLED_SERVICES"
